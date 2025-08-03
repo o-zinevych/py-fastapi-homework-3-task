f6221d9f1195518553ec6a406758aed74b89564d
Olha <olgazinevych@gmail.com>
2025-08-03 17:51:32 +0300

    add token refresh request and response schemas

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -58,3 +58,11 @@ class UserLoginResponseSchema(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
