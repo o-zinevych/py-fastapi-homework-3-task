92c6338f2f91ef77317bfcec25abc05ce99ae7ae
Olha <olgazinevych@gmail.com>
2025-08-03 08:57:44 +0300

    add User Registration Response and Request schemas with custom email and password validation

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,25 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
 
-from database import accounts_validators
+from database import accounts_validators, UserGroupEnum
 
 
-# Write your code here
+class UserRegistrationRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+    role: UserGroupEnum = UserGroupEnum.USER
+
+    @field_validator("email")
+    @classmethod
+    def validate_user_email(cls, user_email: str) -> str:
+        return accounts_validators.validate_email(user_email)
+
+    @field_validator("password")
+    @classmethod
+    def validate_user_password_strength(cls, user_password: str) -> str:
+        return accounts_validators.validate_password_strength(user_password)
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    email: EmailStr
